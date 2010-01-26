b1e366be5f2f84abfc2aadad254fc493848167e8
serninpc <serninpc@d8a8a728-0a5f-11df-8158-c7cbc0dd6240>
2010-01-26 13:30:23 +0000

    Initial RelayLib commit.

---
+++ RelayLib/Version.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paneidos.Relay
+{
+    public class Version
+    {
+        public const string Vendor = "Paneidos Desu";
+        public const string Product = "SFTP-Relay";
+        public const string VersionString = "0.1";
+        public static string[] WelcomeMessage = null;
+        static Version()
+        {
+
+            Version.WelcomeMessage = new string[]  {
+                                                   "-=-=- Welcome to "+Vendor+" "+Product+" "+VersionString+" -=-=-",
+                                                   "This is a relay system, anonymous logins will not work.",
+                                                   "Please login as user@sftp-host, and use your password.",
+                                                   "SSH-keys are not supported yet."
+          };
+        }
+    }
+}
