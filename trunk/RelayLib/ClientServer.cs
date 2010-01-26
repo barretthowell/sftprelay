e227340c5af6401219ea3180881fc380605b5899
serninpc <serninpc@d8a8a728-0a5f-11df-8158-c7cbc0dd6240>
2010-01-26 13:30:23 +0000

    Initial RelayLib commit.

---
+++ trunk/RelayLib/ClientServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Paneidos.Relay
+{
+    
+    public class ClientServer
+    {
+        public delegate void ClientTerminatedHandler(ClientServer cs);
+
+        public event ClientTerminatedHandler ClientTerminated;
+
+
+        private TcpClient client;
+        private StreamReader input;
+        private StreamWriter output;
+        private bool running;
+
+        public ClientServer( TcpClient client)
+        {
+            this.client = client;
+            client.Client.Blocking = false;
+            Stream s = client.GetStream();
+            input = new StreamReader(s);
+            output = new StreamWriter(s);
+        }
+        void ReadLine()
+        {
+            while (running)
+            {
+            }
+        }
+        void Run()
+        {
+            running = true;
+            while (running)
+            {
+                
+            }
+            client.Close();
+            input = null;
+            output = null;
+            client = null;
+            ClientTerminated.Invoke(this);
+        }
+        public void Stop()
+        {
+            running = false;
+        }
+    }
+}
---
+++ trunk/RelayLib/RelayServer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Paneidos.Relay
+{
+    public class RelayServer
+    {
+        private const int DefaultPort = 2121;
+        private static readonly IPAddress DefaultAddress = null;
+        private bool running = false;
+        private Thread serverThread;
+
+        public int Port { get { return localEP.Port; } }
+        public IPAddress Address { get { return localEP.Address; } }
+        public AddressFamily AddressFamily { get { return localEP.AddressFamily; } }
+        
+        private IPEndPoint localEP;
+        private TcpListener serverSocket;
+        private List<ClientServer> pool = new List<ClientServer>();
+
+        static RelayServer()
+        {
+            RelayServer.DefaultAddress = IPAddress.Loopback;
+        }
+        public RelayServer()
+            : this(new IPEndPoint(DefaultAddress, DefaultPort))
+        {
+        }
+
+        public RelayServer(int port)
+            : this(new IPEndPoint(DefaultAddress, port))
+        {
+            
+        }
+        public RelayServer(IPAddress address, int port)
+            : this(new IPEndPoint(address, port))
+        {
+        }
+        public RelayServer(IPEndPoint localEP)
+        {
+            this.localEP = localEP;
+        }
+        public void Start()
+        {
+            serverThread = new Thread(new ThreadStart(Run));
+            serverThread.Start();
+        }
+        public void Stop()
+        {
+            running = false;
+            serverThread.Join();
+        }
+        void Run()
+        {
+            ClientServer curCS;
+            running = true;
+            serverSocket = new TcpListener(localEP);
+            serverSocket.Start();
+            while (running)
+            {
+                while (running && !serverSocket.Pending())
+                {
+                    Thread.Sleep(100);
+                }
+                if (!running) break;
+                curCS = new ClientServer(serverSocket.AcceptTcpClient());
+                curCS.ClientTerminated += new ClientServer.ClientTerminatedHandler(curCS_ClientTerminated);
+                pool.Add(curCS);
+            }
+            serverSocket.Stop();
+            for (int i = pool.Count - 1; i >= 0; i--)
+            {
+                pool[i].Stop();
+            }
+        }
+
+        void curCS_ClientTerminated(ClientServer cs)
+        {
+            //TODO
+        }
+
+    }
+}
